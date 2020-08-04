8cc0c180137c8d50d3be38cd95b370a251720951
Clint Lawson <clint_lwsn@hotmail.com>
2020-08-03 18:09:42 -0600

    C# first proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCSapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
